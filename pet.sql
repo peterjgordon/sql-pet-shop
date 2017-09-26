c6ce0dc1bbe1dd78469d973ebcc46fe5fabdda89
Peter Gordon <p.gordon@kainos.com>
2017-09-26 11:33:08 +0100

    Added NOT NULL to required fields; overwrites existing tables

--- pet.sql
+++ pet.sql
@@ -1,11 +1,25 @@
--- CREATE TABLE species (id AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20);
+CREATE DATABASE IF NOT EXISTS pet-shop;
+DROP TABLE IF EXISTS (species,pet,pet_keeper,keeper);
+
+CREATE TABLE species (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(20) NOT NULL
+);
 insert into species values('Dog');
 insert into species values('Cat');
 insert into species values('Snake');
 insert into species values('Bird');
 insert into species values('Hamster');
 insert into species values('Giraffe');
--- CREATE TABLE pet (id AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20), sex CHAR(1), birth DATE, death DATE, speciesID INT FOREIGN KEY REFERENCES species(id));
+
+CREATE TABLE pet (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(20),
+	sex CHAR(1),
+	birth DATE,
+	death DATE,
+	speciesID INT NOT NULL FOREIGN KEY REFERENCES species(id)
+);
 insert into pet values('Fluffy', 'f', '1993-02-04', null, 2);
 insert into pet values('Claws', 'm', '1994-03-17', null, 2);
 insert into pet values('Buffy', 'f', '1989-05-13', null, 1);
@@ -15,7 +29,12 @@ insert into pet values('Chirpy', null, '1997-12-09', null, 4);
 insert into pet values('Whistler', null, '1997-12-09', null, 4);
 insert into pet values('Slim', 'm', '1996-04-29', null, 3);
 insert into pet values('Puffball', 'f', '1999-03-30', null, 5);
--- CREATE TABLE pet_keeper (petID INT FOREIGN KEY REFERENCES pet(id), keeperID INT FOREIGN KEY REFERENCES keeper(id), PRIMARY KEY(petID, keeperID));
+
+CREATE TABLE pet_keeper (
+	petID INT NOT NULL FOREIGN KEY REFERENCES pet(id),
+	keeperID INT NOT NULL FOREIGN KEY REFERENCES keeper(id),
+	PRIMARY KEY(petID, keeperID)
+);
 insert into pet_keeper values(1,1);
 insert into pet_keeper values(2,2);
 insert into pet_keeper values(3,1);
@@ -25,7 +44,11 @@ insert into pet_keeper values(6,2);
 insert into pet_keeper values(7,2);
 insert into pet_keeper values(8,3);
 insert into pet_keeper values(9,4);
--- CREATE TABLE keeper (id AUTO_INCREMENT PRIMARY KEY, name VARCHAR(20));
+
+CREATE TABLE keeper (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(20) NOT NULL
+);
 insert into keeper values('Harold');
 insert into keeper values('Gwen');
 insert into keeper values('Benny');
